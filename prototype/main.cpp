9495b0e05db69533b531baf14b351f56f54a2e37
yury gorshkov <yury.gorshkov@wartsila.com>
2020-03-25 10:22:14 +0300

    Initial Commit

---
+++ prototype/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prototype
+//
+//  Created by yury on 25/03/2020.
+//  Copyright © 2020 WorthIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
